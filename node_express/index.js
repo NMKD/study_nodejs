4298a086f9c68ce513c3ac734d0ef3db5d85a972
kristina <kistina@localhost>
2022-11-07 20:22:51 +0300

    add express-handlebars

--- node_express/index.js
+++ node_express/index.js
@@ -1,5 +1,26 @@
 const express = require('express')
+const exphbs = require('express-handlebars')
+const path = require('path')
 const app = express()
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+// регистрируем, что в express есть движок express-handlebars, его значение hbs.engine
+app.engine('hbs', hbs.engine) 
+// используем движок
+app.set('view engine', 'hbs')
+// где будут шаблоны
+app.set('views', 'views')
+
+app.get('/', (req, res) => {
+   res.render('index')
+})
+
+app.get('/about', (req, res) => {
+   res.render('about')
+})
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
