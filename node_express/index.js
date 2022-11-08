d80aa42072e692b66a1668fbc5127eecf1b394c5
kristina <kistina@localhost>
2022-11-08 21:08:27 +0300

    add routes & views>partials>>navba & add new list.html - add, courses

--- node_express/index.js
+++ node_express/index.js
@@ -1,24 +1,27 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
+
 const hbs = exphbs.create({
     defaultLayout: 'main',
     extname: 'hbs'
 })
+
 // регистрируем, что в express есть движок express-handlebars, его значение hbs.engine
-app.engine('hbs', hbs.engine) 
+app.engine('hbs', hbs.engine)
 // используем движок
 app.set('view engine', 'hbs')
 // где будут шаблоны
 app.set('views', 'views')
 
-app.get('/', (req, res) => {
-   res.render('index')
-})
+app.use(express.static('public'))
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
 
-app.get('/about', (req, res) => {
-   res.render('about')
-})
 
 const PORT = process.env.PORT || 3000
 
